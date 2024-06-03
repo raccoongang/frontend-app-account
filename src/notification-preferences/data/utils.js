98bb31c6f327013150de6c19891d8321272fc7e0
KyryloKireiev <kirillkireev888@gmail.com>
2024-06-03 18:39:40 +0300

    feat: [AXM-500] Extend Notifications preferences view with Push notifications

--- src/notification-preferences/data/utils.js
+++ src/notification-preferences/data/utils.js
@@ -1,6 +1,10 @@
 import { getConfig } from '@edx/frontend-platform';
 
-export const notificationChannels = () => ({ WEB: 'web', ...(getConfig().SHOW_EMAIL_CHANNEL === 'true' && { EMAIL: 'email' }) });
+export const notificationChannels = () => ({
+  WEB: 'web',
+  ...(getConfig().SHOW_EMAIL_CHANNEL === 'true' && { EMAIL: 'email' }),
+  ...(getConfig().SHOW_PUSH_CHANNEL === 'true' && { PUSH: 'push' }),
+});
 
 export const shouldHideAppPreferences = (preferences, appId) => {
   const appPreferences = preferences.filter(pref => pref.appId === appId);
